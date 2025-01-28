a46e1eb1119509fcafe371d1bb76fb3b8b42f0c3
Harry <104682768+Harry300408@users.noreply.github.com>
2025-01-28 10:49:35 +0000

    freaky

--- Test.py
+++ Test.py
@@ -1,1 +1,10 @@
-hello
+class FreakyRhys():
+    def __init__(self):
+        self.freak = 100
+
+class FreakyAaron(FreakyRhys):
+    def __init__(self):
+        super().__init__()
+
+test = FreakyRhys()
+print(test)
